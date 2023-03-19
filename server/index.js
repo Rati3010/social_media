d348a3545ed14c66629cacc611fdf257e5946b4c
Rati3010 <mratikanta1997@gmail.com>
2023-03-19 21:04:28 +0530

    done authentication part

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
@@ -8,26 +7,46 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { connection } from "./config/db.js";
+import authRoutes from "./routes/auth.js"
 
 // Configuration
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use("/assets",express.static(path.join(__dirname,"public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FIle Storage
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"public/assets")
+    destination: function (req, file, cb) {
+        cb(null, "public/assets")
     },
-    filename: function (req,file,cb){
-        cb(null,file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+//  Routes with files
+app.post('/auth/register', upload.single("picture"), register);
+
+// routes
+app.use("/auth", authRoutes)
+
+//  Mongoose setup
+const port = process.env.port || 8080;
+app.listen(port, async () => {
+    try {
+        await connection;
+        console.log("Connected to DB sucessfully")
+    } catch (error) {
+        console.log(error)
     }
 })
---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const UserSchema  = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        min:2,
+        max:50
+    },
+    lastName:{
+        type:String,
+        required:true,
+        min:2,
+        max:50
+    },
+    email:{
+        type:String,
+        required:true,
+        max:50,
+        unique:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        min:8,
+    },
+    picturePath:{
+        type:String,
+        default:""
+    },
+    friends:{
+         type:Array,
+         default:[]
+    },
+    location:String,
+    occupation:String,
+    viewProfile:Number,
+    impressions:Number
+},{timestamps:true}) 
+
+export const User = mongoose.model("user",UserSchema);
